409393aa3bf6f5c1607b5ce02d589a991743fcca
ethan <esamangy23@wooster.edu>
2024-02-07 15:21:03 -0500

    added shelves. added player collision

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,10 +11,37 @@ public class PlayerController : MonoBehaviour {
     private bool isWalking;
 
     private void Update(){
+        HandleMovement();
+    }
+
+    private void HandleMovement(){
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .5f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if(!canMove){
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove){
+                moveDir = moveDirX;
+            } else {
+                Vector3 moveDirZ = new Vector3(moveDir.x, 0, 0);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if(canMove){
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if(canMove){
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
