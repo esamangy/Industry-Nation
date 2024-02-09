94f52b973fd53efb4ea9ac538233f06af655fdbc
ethan <esamangy23@wooster.edu>
2024-02-08 19:47:59 -0500

    added ability to move and know where factory object is

--- Assets/Scripts/FactoryObject.cs
+++ Assets/Scripts/FactoryObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,29 @@ using UnityEngine;
 public class FactoryObject : MonoBehaviour {
     
     [SerializeField] private FactoryObjectSO factoryObjectSO;
+
+    private MediumShelf mediumShelf;
     public FactoryObjectSO GetFactoryObjectSO(){
         return factoryObjectSO;
+    }
+
+    public void SetMediumShelf(MediumShelf mediumShelf){
+        if(this.mediumShelf != null){
+            this.mediumShelf.ClearFactoryObject();
+        }
+
+        this.mediumShelf = mediumShelf;
+
+        if(mediumShelf.HasFactoryObject()){
+            throw new Exception("Counter already has a FactoryObject!");
+        }
+        mediumShelf.SetFactoryObject(this);
+
+        transform.parent = mediumShelf.GetFactoryObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public MediumShelf GetMediumShelf(){
+        return mediumShelf;
     }
 }
--- Assets/Scripts/Interactables/MediumShelf.cs
+++ Assets/Scripts/Interactables/MediumShelf.cs
@@ -7,11 +7,46 @@ public class MediumShelf : MonoBehaviour {
     [SerializeField] private FactoryObjectSO FactoryObjectSO;
     [SerializeField] private Transform topOfShelf;
     
-    public void Interact(){
-        Debug.Log("Interact!");
-        GameObject pipeTransform = Instantiate(FactoryObjectSO.prefab, topOfShelf);
-        pipeTransform.transform.localPosition = Vector3.zero;
+    private FactoryObject factoryObject;
 
-        pipeTransform.GetComponent<FactoryObject>().GetFactoryObjectSO();
+    //testing
+    [SerializeField] private MediumShelf secondMediumShelf;
+    [SerializeField] private bool testing;
+
+    private void Update() {
+        if(testing && Input.GetKeyDown(KeyCode.T)){
+            if(factoryObject != null){
+                factoryObject.SetMediumShelf(secondMediumShelf);
+            }
+        }
+    }
+    public void Interact(){
+        if(factoryObject == null){
+            GameObject factoryObjectTransform = Instantiate(FactoryObjectSO.prefab, topOfShelf);
+            factoryObjectTransform.GetComponent<FactoryObject>().SetMediumShelf(this);
+        } else {
+
+        }
+        
+    }
+
+    public Transform GetFactoryObjectFollowTransform(){
+        return topOfShelf;
+    }
+
+    public void SetFactoryObject(FactoryObject factoryObject){
+        this.factoryObject = factoryObject;
+    }
+
+    public FactoryObject GetFactoryObject(){
+        return factoryObject;
+    }
+
+    public void ClearFactoryObject(){
+        factoryObject = null;
+    }
+
+    public bool HasFactoryObject(){
+        return factoryObject != null;
     }
 }
